c451af52ec7ebf02b56455b33ec3327aa3f70e66
Sahithi Bellam <sahithibellam@gmail.com>
2020-12-21 15:01:48 +0530

    subs method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -6,7 +6,7 @@ public class Calc {
 		return num1+num2;
 	}
 	public static int subs(int num1,int num2){
-		return num1+num2;
+		return num1-num2;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
