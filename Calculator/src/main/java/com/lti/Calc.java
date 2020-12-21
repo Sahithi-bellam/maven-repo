651eb5fef117e0f7cde7c662027bb0e0b227731d
Sahithi Bellam <sahithibellam@gmail.com>
2020-12-21 14:58:45 +0530

    added subs method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,9 +5,13 @@ public class Calc {
 	public static int add(int num1,int num2){
 		return num1+num2;
 	}
+	public static int subs(int num1,int num2){
+		return num1+num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println(add(10,20));
+    System.out.println("Sum="+add(10,20));
+    System.out.println("Difference="+subs(10,20));
 	}
 
 }
